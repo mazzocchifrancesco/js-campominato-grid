6864e0b0ee5ea5a7eaf9fd26aca6255b238aeea4
FrancescoMazzocchi <123@email.it>
2023-11-07 15:21:39 +0100

    feat: aggiunta colore selezione (toggle)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const start=document.getElementById("start");
 container=document.getElementById("griglia");
+
 start.addEventListener("click", function() {
     generaSq(container, "div", "quadrato1")
     console.log("funziona")
@@ -22,6 +23,10 @@ function generaSq (contenitore, elemento, classe) {
         square.classList.add(classe);
         contenitore.append(square);
         square.innerHTML = i+1;
+        square.addEventListener("click", function() {
+            console.log(i+1);
+            square.classList.toggle("selected");
+        })
     }
 }
 
