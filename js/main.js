8645ff2ddea6f963998e26e2a7ec1fef1cb75246
FrancescoMazzocchi <123@email.it>
2023-11-07 15:59:25 +0100

    feat: aggiunta selettore

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 const start=document.getElementById("start");
 container=document.getElementById("griglia");
+let celle=50;
 
 start.addEventListener("click", function() {
-    generaSq(container, "div", "quadrato1")
-    console.log("funziona")
+    const difficolta=document.getElementById("selettore").value
+    if (difficolta=="dif1") {celle=100;}
+    else if (difficolta=="dif2") {celle=81;}
+    else if (difficolta=="dif3") {celle=49;}
+    generaSq(container, "div", "quadrato", difficolta, celle)
 });
 
 
@@ -17,12 +21,18 @@ start.addEventListener("click", function() {
 
 // FUNZIONI
 
-function generaSq (contenitore, elemento, classe) {
-    for (let i = 0; i < 100; i++) {
+function generaSq (contenitore, elemento, classe1, classe2, celle) {
+    for (let i = 0; i < celle; i++) {
+        // crea elemento
         const square= document.createElement(elemento);
-        square.classList.add(classe);
+        // aggiunge classi
+        square.classList.add(classe1);
+        square.classList.add(classe2);
+        // attacca nel DOM
         contenitore.append(square);
+        // aggiunge scritta
         square.innerHTML = i+1;
+        // aggiunge evento click
         square.addEventListener("click", function() {
             console.log(i+1);
             square.classList.toggle("selected");
